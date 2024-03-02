ca4397b024217b53d84396ea75f71d0e993d2453
Pedro Santiago <pedrossantiago1@gmail.com>
2024-03-02 01:18:17 -0300

    Update schema file path in drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 export default {
-	schema: './src/lib/schema.ts',
+	schema: './src/lib/db/schema.ts',
 	out: './drizzle',
 	driver: 'turso',
 	dbCredentials: {
