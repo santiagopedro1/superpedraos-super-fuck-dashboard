1b6895e615a6233505e7e29f220f4cb500d1c252
Pedro Santiago <santiagopedrodev@gmail.com>
2024-10-19 13:02:56 -0300

    Date picker now supports single date

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,4 @@
-import { CalendarDate } from '@internationalized/date';
+import { CalendarDate, DateFormatter, type DateValue } from '@internationalized/date';
 
 export const currencyFormatter = (n: number) => {
 	const r = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' });
@@ -16,4 +16,20 @@ export const codeMapper = (code: number) => {
 
 export const dateToCalendarDate = (date: Date) => {
 	return new CalendarDate(date.getFullYear(), date.getMonth() + 1, date.getDate());
+};
+
+export const dateFormatter = (
+	date: Date | DateValue,
+	tz: string = 'America/Sao_Paulo',
+	locale: string = 'pt-BR'
+) => {
+	const df = new DateFormatter(locale, {
+		dateStyle: 'long'
+	});
+
+	if ('toDate' in date) {
+		date = date.toDate(tz);
+	}
+
+	return df.format(date);
 };
